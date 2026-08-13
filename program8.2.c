faecfee2652f247c0a394b099210d032901d3363
aseemhingorja139917-beep <aseem.hingorja139917@marwadiuniversity.ac.in>
2026-08-13 12:45:08 +0530

    Add files via upload

---
+++ program8.2.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+void findminmax(int arr[],int n, int index, int *min,int *max);
+
+void main()
+{
+    int arr[100],n, i;
+    int min, max;
+
+    printf("enter the size of array : ");
+    scanf("%d", &n);
+
+
+    printf("enter %d elements : ", n);
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    min = max = arr[0];
+
+    findminmax(arr, n, 1, &min, &max);
+
+    printf("minimum = %d\n", min);
+    printf("maximum = %d\n", max);
+}
+
+void findminmax(int arr[], int n, int index, int *min, int *max)
+{
+     if(index == n)
+     {
+         return;
+     }
+
+     if(arr[index] < *min)
+     {
+        *min = arr[index];
+     }
+
+     findminmax(arr, n, index + 1, min, max);
+}
